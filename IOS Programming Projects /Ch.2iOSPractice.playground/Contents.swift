c257574ad4594753ece36718dcb7e78839d0efe4
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-06-27 14:50:19 -0600

    More progress

--- IOS Programming Projects /Ch.2iOSPractice.playground/Contents.swift
+++ IOS Programming Projects /Ch.2iOSPractice.playground/Contents.swift
@@ -9,3 +9,24 @@ let constStr = str
 var nextYear: Int
 var bodyTemp: Float
 var hasPet: Bool
+var arrayOfInts: [Int]
+var dictionaryOfCapitalsByCountry: [String:String]
+var winningLotteryNumbers: Set<Int>
+let number = 42
+let meaningOfLife = String(number)
+let fmStation = 91.1
+let countingUp = ["one", "two"]
+let nameByParkingSpae = [13:"Alice", 27:"Bob"]
+let secondElement = countingUp[1]
+let emptyString = String()
+let emptyArrayOfInts = [Int]()
+let emptySetOfFloats = Set<Float>()
+let defaultNumber = Int()
+let defaultBool = Bool()
+let availableRooms = Set([205, 411, 412])
+let defaultFloat = Float()
+let floatFromLiteral = Float(3.14)
+let easyPi = 3.14
+let floatFromDouble = Float(easyPi)
+floatFromDouble
+let floatingPi: Float = 3.14
