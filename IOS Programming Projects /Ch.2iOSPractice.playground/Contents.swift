a516d9197ef754bc60f477718dfcdc113425b26f
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-07-05 22:19:09 -0600

    Making some changes

--- IOS Programming Projects /Ch.2iOSPractice.playground/Contents.swift
+++ IOS Programming Projects /Ch.2iOSPractice.playground/Contents.swift
@@ -2,31 +2,42 @@
 
 import UIKit
 
-var str = "Hello, playground"
-str = "Hello, Swift"
-let constStr = str
+//var str = "Hello, playground"
+//str = "Hello, Swift"
+//let constStr = str
+//
+//var nextYear: Int
+//var bodyTemp: Float
+//var hasPet: Bool
+//var arrayOfInts: [Int]
+//var dictionaryOfCapitalsByCountry: [String:String]
+//var winningLotteryNumbers: Set<Int>
+//let number = 42
+//let meaningOfLife = String(number)
+//let fmStation = 91.1
+//let countingUp = ["one", "two"]
+//let nameByParkingSpae = [13:"Alice", 27:"Bob"]
+//let secondElement = countingUp[1]
+//let emptyString = String()
+//let emptyArrayOfInts = [Int]()
+//let emptySetOfFloats = Set<Float>()
+//let defaultNumber = Int()
+//let defaultBool = Bool()
+//let availableRooms = Set([205, 411, 412])
+//let defaultFloat = Float()
+//let floatFromLiteral = Float(3.14)
+//let easyPi = 3.14
+//let floatFromDouble = Float(easyPi)
+//floatFromDouble
+//let floatingPi: Float = 3.14
 
-var nextYear: Int
-var bodyTemp: Float
-var hasPet: Bool
-var arrayOfInts: [Int]
-var dictionaryOfCapitalsByCountry: [String:String]
-var winningLotteryNumbers: Set<Int>
-let number = 42
-let meaningOfLife = String(number)
-let fmStation = 91.1
-let countingUp = ["one", "two"]
-let nameByParkingSpae = [13:"Alice", 27:"Bob"]
-let secondElement = countingUp[1]
-let emptyString = String()
-let emptyArrayOfInts = [Int]()
-let emptySetOfFloats = Set<Float>()
-let defaultNumber = Int()
-let defaultBool = Bool()
-let availableRooms = Set([205, 411, 412])
-let defaultFloat = Float()
-let floatFromLiteral = Float(3.14)
-let easyPi = 3.14
-let floatFromDouble = Float(easyPi)
-floatFromDouble
-let floatingPi: Float = 3.14
+//Properties
+//var countingUp = ["one", "two"]
+//let secondElement = countingUp[1]
+//countingUp.count
+//countingUp.append("three")
+//let emptyString = ""
+//emptyString.isEmpty
+
+//Optionals
+var anOptionalFloat: Float?
---
+++ Playground Practice/WeLoveSwiftExercises.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//1.1
+//var a = 2
+//var b = 3
+//var c = a + b
+
+//1.2
+//var secondsInAYear = 60*60*24*365
+
+//1.3
+//var height = 1920
+//var width = 1080
+//var numberOfPixels = height*width
+
+//1.4
+
+let a = 10
+let b = 6
+let sum = a + b
+let difference = a - b
