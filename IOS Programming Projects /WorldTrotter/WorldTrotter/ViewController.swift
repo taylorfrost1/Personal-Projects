b7566995ac971e97a9b8a00520cb3d29edf129cd
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-07-12 20:38:08 -0600

    Done for the night, got first function in.

--- IOS Programming Projects /WorldTrotter/WorldTrotter/ViewController.swift
+++ IOS Programming Projects /WorldTrotter/WorldTrotter/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blueColor()
         
     }
 
