e044edc82ca4736c161c31ff48c6de00e65476a3
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-07-12 20:19:11 -0600

    Started new project "GlobeTrotter"

--- IOS Programming Projects /Ch.2iOSPractice.playground/Contents.swift
+++ IOS Programming Projects /Ch.2iOSPractice.playground/Contents.swift
@@ -41,3 +41,26 @@ import UIKit
 
 //Optionals
 var anOptionalFloat: Float?
+var anOptionalArrayOfStrings: [String]?
+var anOptionalArrayOfOptionalStrings: [String]?
+
+var reading1: Float?
+var reading2: Float?
+var reading3: Float?
+reading1 = 9.8
+reading2 = 9.2
+reading3 = 9.7
+
+if let r1 = reading1,
+    r2 = reading2,
+    r3 = reading3 {
+        let avgReading = (r1 + r2 + r3) / 3
+} else {
+    let errorString = "Instrument reported a reading that was nil"
+}
+
+let nameByParkingSpace = [13: "Alice", 27: "Bob"]
+let space13Assignee: String? = nameByParkingSpace[13]
+let space42Assignee: String? = nameByParkingSpace[42]
+
+
---
+++ IOS Programming Projects /WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Taylor Frost on 7/12/16.
+//  Copyright © 2016 Taylor Frost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
